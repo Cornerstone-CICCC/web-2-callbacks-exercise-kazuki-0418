8dc9ff8835c74a4337359d123f932969cf763512
kazuki-0418 <kazuki.castle0418@gmail.com>
2024-11-19 15:38:59 -0800

    Exercise is done

--- file_reader.js
+++ file_reader.js
@@ -1,5 +1,29 @@
 const fs = require("fs");
 
-fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
-  // your code here
-})
+
+function readFile(file) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
+    });
+  });
+}
+
+const files = ["firstname.txt", "lastname.txt", "age.txt", "hobbies.txt"];
+
+(async function (files) {
+  try {
+    const firstName = await readFile(files[0]);
+    const lastName = await readFile(files[1]);
+    const age = await readFile(files[2]);
+    const hobbies = await readFile(files[3]);
+    const hobbiesArr = await JSON.parse(hobbies)
+    
+    console.log(`${firstName} ${lastName} is ${age} years old and his hobbies are ${hobbiesArr.join(" and ")}`);
+  } catch (err) {
+    console.log(err);
+  }
+})(files);
